8f749a2aa478f138dd31377269c5a67341792aa6
SaaD Ibne Jamal <shaad036@gmail.com>
2023-01-20 01:03:21 +0600

    create sidebar

---
+++ src/components/layout/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Sidebar from './sidebar';
+
+function Layout() {
+    return (
+        <div style={{ height: '100vh' }}>
+            <Sidebar />
+        </div>
+    );
+}
+
+export default Layout;
---
+++ src/components/layout/sidebar/index.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+
+import styles from './styles.module.css';
+
+function Sidebar() {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setIsSidebarOpen(open);
+    };
+
+    const list = (anchor) => (
+        <Box
+            className={styles.sidebarBox}
+            role="presentation"
+            onKeyDown={toggleDrawer(anchor, false)}
+        >
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
+
+    return (
+        <div className={styles.sibebarContainer}>
+            <Button
+                onClick={toggleDrawer('right', true)}
+                className={styles.sidebarOpenButton}
+            >
+                <ArrowCircleLeftIcon className={styles.sidebarOpenIcon}/>
+            </Button>
+            <Drawer
+                anchor='right'
+                open={isSidebarOpen}
+                transitionDuration={850}
+                onClose={toggleDrawer('right', false)}
+            >
+                {list('right')}
+            </Drawer>
+        </div>
+    );
+}
+
+export default Sidebar;
