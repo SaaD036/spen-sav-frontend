a82a37ba34c3846fbf569f9ba8e769d3ca9dc7af
SaaD Ibne Jamal <shaad036@gmail.com>
2023-09-06 02:19:26 +0600

    Add env files and prepare project to deploy

--- src/utilities/url/index.js
+++ src/utilities/url/index.js
@@ -1,7 +1,11 @@
-export const BASE_URL = 'http://localhost:8000/api/';
+export const BASE_URL = process.env.NODE_ENV === '"development"'
+    ? process.env.REACT_APP_DEV_URL
+    : process.env.REACT_APP_PROD_URL;
 
 export const buildURL = (path, variables = {}, params = {}) => {
     let url = `${BASE_URL}${path}`;
+
+    console.log('SaaD : ', process.env);
 
     Object.keys(variables).forEach(key => {
         url = url.replace(`{${key}}`, variables[key]);
