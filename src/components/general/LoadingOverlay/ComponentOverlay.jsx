231d7c4b6efac3f4b8d00fb8d57723617bc5003a
SaaD036 <shaad036@gmail.com>
2024-10-19 21:17:07 +0600

    Modify page loader component

--- src/components/general/LoadingOverlay/ComponentOverlay.jsx
+++ src/components/general/LoadingOverlay/ComponentOverlay.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './styles.module.css';
 
-const ComponentOverlay = () => {
-    return (
-        <div className={styles.loader}></div>
-    );
+const ComponentOverlay = (props) => {
+    const { message } = props;
+
+    return <div></div>;
 };
+
+ComponentOverlay.propTypes = {
+    message: PropTypes.string,
+};
+
+ComponentOverlay.defaultProps = {};
 
 export default ComponentOverlay;
--- src/components/users/index.jsx
+++ src/components/users/index.jsx
@@ -2,12 +2,7 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import {
-    Input,
-    Grid,
-    Tooltip,
-    Container,
-} from '@mui/material';
+import { Input, Grid, Tooltip, Container } from '@mui/material';
 import {
     Email as EmailIcon,
     Visibility as ViewerIcon,
@@ -24,18 +19,17 @@ import UserDrawer from '../general/Drawer/UserDrawer';
 import { userRole } from './../../constants/user';
 import { getCustomListToRender } from '../../utilities/customListUtilities';
 import styles from './styles.module.css';
+import LoadingOverlay from '../general/LoadingOverlay';
 
-const Users = props => {
-    const {
-        users,
-        getAllUsers,
-    } = props;
+const Users = (props) => {
+    const { users, getAllUsers } = props;
 
     const [name, setName] = useState('');
     const [userId, setUserId] = useState();
     const [isUserDrawerOpen, setIsUserDrawerOpen] = useState(false);
+    const [isPageLoading, setIsPageLoading] = useState(false);
 
-    const onNameTextChange = e => {
+    const onNameTextChange = (e) => {
         // setName(e.target.value.trim());
         setName(name);
     };
@@ -43,6 +37,12 @@ const Users = props => {
     const onUserNameClick = (userId) => {
         setUserId(userId);
         setIsUserDrawerOpen(true);
+    };
+
+    const loadUserData = async () => {
+        setIsPageLoading(true);
+        await getAllUsers();
+        setIsPageLoading(false);
     };
 
     const renderUserDrawer = () => {
@@ -72,62 +72,65 @@ const Users = props => {
         );
     };
 
-    const renderUserListHeader = user => {
+    const renderUserListHeader = (user) => {
         return (
             <div className={styles.userListHeader}>
                 <b style={{ cursor: 'pointer' }} onClick={() => onUserNameClick(user._id)}>
                     {`${user.firstName} ${user.lastName}`}
                 </b>
-                <VerticalDotIcon className={styles.headerOptionIcon}/>
+                <VerticalDotIcon className={styles.headerOptionIcon} />
             </div>
         );
     };
 
-    const renderUserListBody = user => {
+    const renderUserListBody = (user) => {
         return (
-            <Grid direction='column' className={styles.userListBody}>
+            <Grid direction="column" className={styles.userListBody}>
                 <div className={styles.userListBodyItem}>
-                    <EmailIcon fontSize='small' className={styles.userListEmailIcon} />
-                    <span>
-                        { user.email }
-                    </span>
+                    <EmailIcon fontSize="small" className={styles.userListEmailIcon} />
+                    <span>{user.email}</span>
                 </div>
                 <div className={styles.userListBodyItem}>
                     <Tooltip title="User role">
-                        {user.role === userRole.ADMIN
-                            ? <AdminIcon fontSize='small' className={styles.userListEmailIcon} />
-                            : <ViewerIcon fontSize='small' className={styles.userListEmailIcon} />
-                        }
+                        {user.role === userRole.ADMIN ? (
+                            <AdminIcon fontSize="small" className={styles.userListEmailIcon} />
+                        ) : (
+                            <ViewerIcon fontSize="small" className={styles.userListEmailIcon} />
+                        )}
                     </Tooltip>
-                    <span>
-                        { user.role }
-                    </span>
+                    <span>{user.role}</span>
                 </div>
                 <div className={styles.userListBodyItem}>
                     <Tooltip title="Total entry">
-                        <EntryIcon fontSize='small' className={styles.userListEmailIcon} />
+                        <EntryIcon fontSize="small" className={styles.userListEmailIcon} />
                     </Tooltip>
-                    <span>
-                        { user.entryCount }
-                    </span>
+                    <span>{user.entryCount}</span>
                 </div>
             </Grid>
         );
     };
 
     useEffect(() => {
-        getAllUsers();
+        loadUserData();
     }, []);
 
     return (
         <div className={styles.usersPageContainer}>
             {userId && renderUserDrawer()}
             {renderSearchFields()}
-            <Container className={styles.userListContainer}>
-                <CustomList
-                    lists={getCustomListToRender(users, renderUserListHeader, renderUserListBody)}
-                />
-            </Container>
+            {isPageLoading ? (
+                <LoadingOverlay />
+            ) : (
+                <Container className={styles.userListContainer}>
+                    <CustomList
+                        lists={getCustomListToRender(
+                            users,
+                            renderUserListHeader,
+                            renderUserListBody,
+                        )}
+                    />
+                </Container>
+            )}
         </div>
     );
 };
@@ -143,7 +146,7 @@ Users.defaultProps = {
     totalUser: 0,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
     users: state.users.users,
     totalUser: state.users.totalUser,
 });
